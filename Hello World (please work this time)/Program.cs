94cae76295519f7cbfdea1de7a3253a58de5e4f4
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-09-12 15:02:46 -0300

    Added Console Log
    "Hello World"

--- Hello World (please work this time)/Program.cs
+++ Hello World (please work this time)/Program.cs
@@ -10,6 +10,8 @@ namespace Hello_World__please_work_this_time_
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
